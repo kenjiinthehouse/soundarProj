8cedc1b3aaeb5a489c4311508cab551fe90b81d3
rere2133 <rere2133@users.noreply.github.com>
2020-11-12 00:12:50 +0800

    新增product api 搜尋、排序、篩選、分類

--- Node/src/db_connect.js
+++ Node/src/db_connect.js
@@ -2,22 +2,22 @@ require("dotenv").config();
 const mysql2 = require("mysql2");
 
 const pool = mysql2.createPool({
+  host: "localhost",
+  user: "jl55661688",
+  password: "iouccc19931107",
+  database: "mfee09_project",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+
+  //reckie
   // host: "localhost",
-  // user: "jl55661688",
-  // password: "iouccc19931107",
+  // user: "root",
+  // password: "",
   // database: "mfee09_project",
   // waitForConnections: true,
   // connectionLimit: 10,
   // queueLimit: 0,
-
-  //reckie
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "pd_test",
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
 });
 
 // const pool = mysql2.createPool({
