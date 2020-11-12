eb8ec78fdd14e40dc8ad40428e8cd15961c3981e
Jaycub <jl55661688@yahoo.com.tw>
2020-11-12 14:42:20 +0800

    完成audio收藏

--- reactproj/src/App.js
+++ reactproj/src/App.js
@@ -130,7 +130,10 @@ function App() {
                 />
               </Route>
               <Route path="/audiocollect">
-                <Audiocollection />
+                <Audiocollection
+                  globalAudioArry={globalAudioArry}
+                  setGlobalAudioArry={setGlobalAudioArry}
+                />
               </Route>
               <Route path="/channelcollect">
                 <Channelcollection />
