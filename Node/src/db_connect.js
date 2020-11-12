cfc79709f49ee41b487938b53b38ae29475d8c28
kenjiinthehouse <69293355+kenjiinthehouse@users.noreply.github.com>
2020-11-12 20:55:52 +0800

    ripple css/navbar member collapese

--- Node/src/db_connect.js
+++ Node/src/db_connect.js
@@ -2,22 +2,22 @@ require("dotenv").config();
 const mysql2 = require("mysql2");
 
 const pool = mysql2.createPool({
-  host: "localhost",
-  user: "jl55661688",
-  password: "iouccc19931107",
-  database: "mfee09_project",
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-
-  // kenji
   // host: "localhost",
-  // user: "root",
-  // password: "",
+  // user: "jl55661688",
+  // password: "iouccc19931107",
   // database: "mfee09_project",
   // waitForConnections: true,
   // connectionLimit: 10,
   // queueLimit: 0,
+
+  // kenji
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "mfee09_project",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 
   //reckie
   // host: "localhost",
