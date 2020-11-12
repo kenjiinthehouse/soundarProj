373e91990356017ee403934e655568d83ba0ebfb
Shang-Chieh <sky800227@gmail.com>
2020-11-12 18:41:53 +0800

    1112

--- Node/src/db_connect.js
+++ Node/src/db_connect.js
@@ -2,14 +2,51 @@ require("dotenv").config();
 const mysql2 = require("mysql2");
 
 const pool = mysql2.createPool({
+  // host: "localhost",
+  // user: "jl55661688",
+  // password: "iouccc19931107",
+  // database: "mfee09_project",
+  // waitForConnections: true,
+  // connectionLimit: 10,
+  // queueLimit: 0,
+
+  // kenji
+  // host: "localhost",
+  // user: "root",
+  // password: "",
+  // database: "mfee09_project",
+  // waitForConnections: true,
+  // connectionLimit: 10,
+  // queueLimit: 0,
+
+  //reckie
+  // host: "localhost",
+  // user: "root",
+  // password: "",
+  // database: "mfee09_project",
+  // waitForConnections: true,
+  // connectionLimit: 10,
+  // queueLimit: 0,
+
+  // chieh
   host: "localhost",
-  user: "jl55661688",
-  password: "iouccc19931107",
+  user: "chieh",
+  password: "chieh",
   database: "mfee09_project",
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
 });
+
+// const pool = mysql2.createPool({
+//   host: "localhost",
+//   user: "jl55661688",
+//   password: "iouccc19931107",
+//   database: "mfee09_project",
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0,
+// });
 
 // const pool = mysql2.createPool({
 //   host: "localhost",
