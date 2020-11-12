5549bc29b41b23c52beaa85767cac76adead7e5d
rere2133 <rere2133@users.noreply.github.com>
2020-11-12 18:59:16 +0800

    rrrrr

--- reactproj/src/reckie/components/Pdlist/TabBar.js
+++ reactproj/src/reckie/components/Pdlist/TabBar.js
@@ -136,8 +136,17 @@ function TabBar(props) {
           />
           {/* <PdContent 
           {...props}
-          
-          />
+          viewFilter={viewFilter}
+          /> */}
+
+          {/* 活動 */}
+          {/* <AcContent 
+            viewFilter={viewFilter}
+          /> */}
+
+          {/* 錄音室 */}
+          <StudioContent />
+
         </div>
       </TabPanel>
     </div>
