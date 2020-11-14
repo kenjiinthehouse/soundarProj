496eeedeb91df20f240f9a562c493a7cc5c94bf8
Shang-Chieh <sky800227@gmail.com>
2020-11-14 20:25:57 +0800

    博伸-活動寄信功能

--- Node/src/routes/mail.js
+++ Node/src/routes/mail.js
@@ -135,64 +135,51 @@ router.post("/emailcheck", async (req, res) => {
   // res.json(output);
 });
 
-// router.post("/api/email", async (req, res) => {
-//   const output = {
-//     body: req.body,
-//     success: false,
-//     message: "",
-//   };
+//activity
 
-//   let email = req.body.email; //剛剛從前臺傳過來的郵箱
-//   let user_name = req.body.user_name; //剛剛從前臺傳過來使用者名稱
-//   let code = createSixNum();
-//   let date = new Date(); //獲取當前時間
+router.post("/activitymail", async (req, res) => {
+  const output = {
+    body: req.body,
+    success: false,
+    message: "",
+  };
 
-//   let isLive = "no";
+  const account = req.body.account;
 
-//   const sql =
-//     "SELECT `sid`, `account`, `code` FROM `membertest` WHERE account=? AND name=?";
-//   const [rs] = await db.query(sql, [email, user_name]);
-//   if (rs.length) {
-//     output.success = false;
-//     output.message = "帳號已經存在";
-//     res.json(output);
-//   } else {
-//     output.success = true;
-//     output.message = "帳號可行";
-//     let mailOptions = {
-//       // 發件人
-//       from: "podcasttest168@gmail.com",
-//       // 主題
-//       subject: "接受憑證", //郵箱主題
-//       // 收件人
-//       to: email, //前臺傳過來的郵箱
-//       // 郵件內容，HTML格式
-//       text: "用" + code + "作為你的驗證碼", //傳送驗證碼
-//     };
-
-//     transporter.sendMail(mailOptions, function (error, info) {
-//       if (error) {
-//         console.log(error);
-//       } else {
-//         console.log("Email sent: " + info.response);
-//       }
-//     }); //傳送郵件
-//   }
-
-//   res.json(req.body);
-// });
-
-// router.get("/mailtest1", (req, res) => {
-//   res.render("mailtest1", { name: "mic" });
-// });
+  let mailOptions = {
+    // 發件人
+    from: "podcasttest168@gmail.com",
+    // 主題
+    subject: "soundar訂單確認", //郵箱主題
+    // 收件人
+    to: account,
+    text: `\t訂單編號：20201120001T0001F \n
+    \t訂單項目：跟上影音新浪潮｜Podcast企劃+影音內容+影音行銷 \n
+    \t訂單方案：早鳥票 \n
+    \t訂單數量：1張 \n
+    \t訂單金額：TWD$4000 \n
+    \t取票方式：台灣全家 FamiPort 取票 \n
+    \t付款方式：ATM 虛擬帳號 \n
+    \t銀行帳號：(808)9555831823044820 \n
+    \t本虛擬帳號僅對應到您的報名資料，請勿重複轉入到同一帳號 \n
+    \t分行名稱：玉山銀行營業部 \n
+    \t帳戶名稱：Soundar娛樂股份有限公司 \n
+    \t請於24小時內完成轉帳，如超過時限，系統將會取消您的訂單。\n
+    `,
+  };
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      output.message = "郵件無法成功發出去，請確認郵件格式是否正確";
+      console.log(error);
+      res.json(output);
+    } else {
+      output.success = true;
+      output.message = "已成功發送活動訂單，請查看信箱";
+      console.log("Email sent: " + info.response);
+      console.log("output", output);
+      res.json(output);
+    }
+  });
+});
 
 module.exports = router;
-
-// module.exports = function (mail) {
-//   transporter.sendMail(mail, function (error, info) {
-//     if (error) {
-//       return console.log(error);
-//     }
-//     console.log("mail sent:", info.response);
-//   });
-// };
