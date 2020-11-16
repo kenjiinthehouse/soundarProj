f9cbc4a59930691ced5429e49bea2020c10de668
ruby <jenny56292001@gmail.com>
2020-11-16 11:56:23 +0800

    change login modal css

--- reactproj/src/jay/jay_components/InformLoginModal.js
+++ reactproj/src/jay/jay_components/InformLoginModal.js
@@ -16,23 +16,24 @@ function InformLoginModal(props) {
         centered
       >
         <Modal.Body
-          style={{ height: 'auto', padding: '0px' }}
+          style={{ height: 'auto', padding: '0px'}}
           className=" text-center"
         >
+        <div style={{backgroundColor:'#2690DF',color:'#fff',margin: '0px',padding:'10px 0'}}>
+          <h4 class="alert-heading pt-2">請先登入</h4>
+        </div>
           <div
-            class="alert alert-success"
+            class="alert alert-light"
             role="alert"
-            style={{ margin: '0px' }}
+            style={{ margin: '0px'}}
           >
-            <h5 class="alert-heading pt-2">請先登入</h5>
-            <hr />
             <h5
               class="alert-heading"
               onClick={() => {
                 props.history.push(`/login`);
                 setShowInformLoginModal(false);
               }}
-              style={{ cursor: 'pointer' }}
+              style={{ cursor: 'pointer',margin: '0px',padding:'10px 0' }}
             >
               點我前往登入頁面
             </h5>
